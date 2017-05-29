e19336783af5ec79e50c3f1dd75c58b9ced61e73
Ksenia Samsonova <skv199501@gmail.com>
2017-05-29 19:59:22 +0300

    packages

---
+++ app/src/main/java/com/ksenia/dictionary/business/dictionary/DictionaryInteractor.java
@@ -0,0 +1,8 @@
+package com.ksenia.dictionary.business.dictionary;
+
+/**
+ * Created by Ksenia on 28.05.2017.
+ */
+
+public class DictionaryInteractor implements IDictionaryInteractor {
+}
---
+++ app/src/main/java/com/ksenia/dictionary/data/repository/dictionary/IDictionaryRepository.java
@@ -0,0 +1,8 @@
+package com.ksenia.dictionary.data.repository.dictionary;
+
+/**
+ * Created by Ksenia on 28.05.2017.
+ */
+
+public interface IDictionaryRepository {
+}
